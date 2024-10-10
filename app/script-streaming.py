e8c2f7f8e218cc2e255fbe0d32f72ee2699cfa34
kenuhn Rimbert <kenuhn@gmail.com>
2024-10-10 12:27:02 +0200

    send to hive srcipt-streaming

--- app/script-streaming.py
+++ app/script-streaming.py
@@ -1,8 +1,10 @@
 import json
 
+from pyhive import hive
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, struct, to_json
 from pyspark.sql.types import StringType, StructField, StructType
+from sqlalchemy import create_engine
 
 # 1. Initialisation de la session Spark avec support pour Hive
 spark = SparkSession \
@@ -10,6 +12,18 @@ spark = SparkSession \
     .appName("KafkaSparkStreaming") \
     .enableHiveSupport() \
     .getOrCreate()
+
+hive_host = 'hive-server'
+hive_port = 10000
+hive_database = 'message_bot'
+
+# Create a connection to Hive
+conn = hive.Connection(
+    host=hive_host,
+    port=hive_port,
+    database=hive_database
+)
+
 
 # Configuration des logs
 spark.sparkContext.setLogLevel("WARN")
@@ -36,6 +50,10 @@ schema = StructType([
     StructField("value", StringType(), True)
 ])
 
+
+# Create a cursor
+cursor = conn.cursor()
+
 # 5. Transformation des données - Parsing des messages JSON
 df_parsed = df_kafka.withColumn("jsonData", from_json(col("value"), schema)).select("jsonData.*")
 
@@ -46,17 +64,33 @@ df_transformed = df_parsed.withColumn("message", col("message"))
 # Reformater les colonnes dans un format JSON: jsonify({'message': typologie, 'value': value})
 df_jsonified = df_transformed.select(col("message"), col("value"))
 
-# 7. Écriture des données dans Hive
-# Création d'une table Hive (si elle n'existe pas déjà)
-spark.sql("CREATE TABLE IF NOT EXISTS kafka_data (message STRING, value STRING) STORED AS PARQUET")
+create_table_query = """
+CREATE TABLE IF NOT EXISTS comments (
+    message string
+    value string
+)
+STORED AS PARQUET
+"""
+cursor.execute(create_table_query)
 
-# Écriture continue des données dans la table Hive
-query = df_jsonified.writeStream \
+
+insert_query = """
+INSERT INTO TABLE comments VALUES (%s, %s)
+"""
+
+cursor.execute(insert_query, df_jsonified)
+
+query = df_transformed \
+    .writeStream \
     .outputMode("append") \
     .format("hive") \
     .option("checkpointLocation", "/tmp/checkpoints") \
-    .option("path", "/user/hive/warehouse/kafka_data") \
-    .toTable("kafka_data")  # Nom de la table Hive
+    .toTable("comments")  
 
-# 8. Attendre que la stream finisse
+# 9. Attente de la terminaison du streaming
 query.awaitTermination()
+
+print("User data inserted successfully into Hive table.")
+
+
+
